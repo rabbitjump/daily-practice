1f0387430b436983a26ec7370a2488b2879fe46b
rabbitjump <rabbit@rabbitdeMacBook-Air.local>
2016-05-31 01:18:44 +0800

    为什么只要给m1定义形参m1的代码就会自动执行？例如m1(maxWords){}

--- model.js
+++ model.js
@@ -1,30 +1,35 @@
 $(document).ready(function(){
 	//点击提交功能
-	$('input').click(commentSubmit);
-	function commentSubmit() {
-	var content = $('#comment').val();//获取输入内容
-	var p_length = $("p").length;//总评论数
-	if (content.length>0 & content.length<=5) {//输入不为空且小于5个字。
-		if (content.match(/^\s+$/g)) {//内空不能全为空格
-			alert("请输入评论内容");
-		}
-		else {
-			if (p_length==0) {
-				$("h1").after("<p>" + "沙发：" + content + "</p>");//判断是否第一条评论
+	var commentSubmit = (function () {
+		var m1 = function() {
+			var content = $('#comment').val();//获取输入内容
+			var p_length = $("p").length;//总评论数
+			if (content.length>0 & content.length<=5) {//输入不为空且小于5个字。
+				if (content.match(/^\s+$/g)) {//内空不能全为空格
+					alert("请输入评论内容");
+				}
+				else {
+					if (p_length==0) {
+						$("h1").after("<p>" + "沙发：" + content + "</p>");//判断是否第一条评论
+					}
+					else{
+						$("p:last").after("<p>" + (p_length+1) + "楼：" + content + "</p>");
+					}
+					$('#comment').val("");//每次提交完后清除文本框内容
+				}
 			}
-			else{
-				$("p:last").after("<p>" + (p_length+1) + "楼：" + content + "</p>");
+			else if (content.length>5) {
+				alert("只能输入"+5+"个字以内的评论");
 			}
-			$('#comment').val("");//每次提交完后清除文本框内容
+			else {
+				alert("请输入评论内容");
+			};
 		}
-	}
-	else if (content.length>5) {
-		alert("只能输入5个字以内的评论");
-	}
-	else {
-		alert("请输入评论内容");
-	};
-  }
+
+    return {m1:m1};
+	})();
+
+	$('input').click(commentSubmit.m1);
 
 
 
