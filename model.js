d2a16a53c7d1603e003dcb6b6529c84016c4bb0b
rabbitjump <rabbit@rabbitdeMacBook-Air.local>
2016-06-01 01:02:00 +0800

    增加显示用户名，评论时间，点赞，评论总数统计，

--- model.js
+++ model.js
@@ -1,35 +1,49 @@
 $(document).ready(function(){
 	//点击提交功能
 	var commentSubmit = (function () {
-		var m1 = function() {
+		var m1 = function(maxwords) {
+			return function() {
 			var content = $('#comment').val();//获取输入内容
-			var p_length = $("p").length;//总评论数
-			if (content.length>0 & content.length<=5) {//输入不为空且小于5个字。
+			var li_length = $("li").length;//总评论数
+			var time = new Date();//当前时间
+			var goodBad = function(){//点赞函数
+				var count = $(this).find("span").text();//为什么用$(this).eq(1)不行？
+				$(this).find("span").text(parseInt(count)+1);
+
+			}
+			if (content.length>0 & content.length<=maxwords) {//输入不为空且小于5个字。
 				if (content.match(/^\s+$/g)) {//内空不能全为空格
-					alert("请输入评论内容");
+					alert("请输入评论内容1");
 				}
 				else {
-					if (p_length==0) {
-						$("h1").after("<p>" + "沙发：" + content + "</p>");//判断是否第一条评论
+					if (li_length==0) {//判断是否第一条评论
+						$("ul.board").append("<li>" + "<p>"+"用户名"+"<span class='time'>"+time.toLocaleString()+"</span>"+"</p>" + "<div>"+"<b>"+"<i>"+"赞"+"</i>"+"<span>"+0+"</span>"+"</b>"+"<b>"+"<i>"+"踩"+"</i>"+"<span>"+0+"</span>"+"</b>"+"</div>"+"<p>"+"沙发："+content+"</p>" + "</li>");//这堆东西怎么简化？
+						$("b.count").text(li_length+1);//评论数量统计
+						$('b').click(goodBad);
 					}
 					else{
-						$("p:last").after("<p>" + (p_length+1) + "楼：" + content + "</p>");
+						$("li:last").after("<li>" + "<p>"+"用户名"+"<span class='time'>"+time.toLocaleString()+"</span>"+"</p>" + "<div>"+"<b>"+"<i>"+"赞"+"</i>"+"<span>"+0+"</span>"+"</b>"+"<b>"+"<i>"+"踩"+"</i>"+"<span>"+0+"</span>"+"</b>"+"</div>"+"<p>"+(li_length+1)+"楼："+content+"</p>" + "</li>");
+						$("b.count").text(li_length+1);
+						$('b').click(goodBad);
 					}
 					$('#comment').val("");//每次提交完后清除文本框内容
 				}
 			}
-			else if (content.length>5) {
-				alert("只能输入"+5+"个字以内的评论");
+			else if (content.length>maxwords) {
+				alert("只能输入"+maxwords+"个字以内的评论");
 			}
 			else {
-				alert("请输入评论内容");
+				alert("请输入评论内容2");
 			};
 		}
+	}
 
     return {m1:m1};
 	})();
 
-	$('input').click(commentSubmit.m1);
+
+	$('input').click(commentSubmit.m1(100));
+
 
 
 
